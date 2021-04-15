77356b458aeaeec25df0112ae27a7c2d2c129359
Balaji <43308457+baaji29@users.noreply.github.com>
2021-04-15 22:09:11 +0530

    Routing_Changes

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -10,13 +10,22 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListComponent } from './list/list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailsComponent,
+    ListComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +33,7 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
